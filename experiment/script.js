0eebe244f33210d9bc5e2ea30acaabe633005021
Tikam Singh Alma <timonalma81@gmail.com>
2018-09-14 23:09:45 +0530

    h

--- experiment/script.js
+++ experiment/script.js
@@ -1,4 +1,14 @@
-windows.onload = function(){
+window.onload = function(){
  
-    const  pi = 
+    const  pi = 3.142;
+    console.log(pi);
+
+
+var x = 100;
+
+if(x>5){
+    let x=5;
+    console.log("inside_locally:"+x);
+}
+    console.log("outside_globally_locked:"+x);
 }
