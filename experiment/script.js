73410ae5525fda479322eb1bec5576f0e3382059
Tikam Singh Alma <timonalma81@gmail.com>
2018-09-15 09:01:49 +0530

    basics es6

--- experiment/script.js
+++ experiment/script.js
@@ -1,14 +1,54 @@
 window.onload = function(){
+
+
+
+//---------------------------Template Strings----------------------
+    // function logninja(name,age){
+    //     console.log('my name is ${name} and my age is ${age}');
+    
+    // }
+    // logninja("Tikam",21);
+    // } 
  
-    const  pi = 3.142;
-    console.log(pi);
+
+    //------------------------------------___Const----------------------------------------
+//     const  pi = 3.142;
+//     console.log(pi);
+
+//---------------------------------------------Let---------------------------------------
+// var x = 100;
+
+// if(x>5){
+//     let x=5;
+//     console.log("inside_locally:"+x);
+// }
+//     console.log("outside_globally_locked:"+x);
+// }
+
+// var items = document.getElementsByTagName("li");
+
+// for(let x=0; x<items.length; x++){
+//     items[x].onclick = function(){
+//         console.log(x);
+//     }
+// }
+// }
 
 
-var x = 100;
+//-----------------------------Object Literal enhancement-------------------------------- 
 
-if(x>5){
-    let x=5;
-    console.log("inside_locally:"+x);
-}
-    console.log("outside_globally_locked:"+x);
-}
+// var name="Ninja";
+// var belt="Black";
+
+// var ninja =  {
+//     name,belt,
+//     chop(x){
+//         console.log('you chopped my head ${x} times')
+//     }
+// }
+// console.log(ninja.chop(6));
+// }
+
+//-----------------------------------------New String methods---------------------------
+var str="AAAA";
+console.log()
