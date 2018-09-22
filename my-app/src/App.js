c40f048f9799062040d8b828d32c50c32b7a4bcb
Tikam02 <timonalma81@gmail.com>
2018-09-23 01:14:15 +0530

    jsx

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>hii i'm react app</h1>
+        <Person name="Max" age="28"></Person>
+        <Person name="manu" age="29">My Hobbies : racing</Person>
       </div>
     );
+    // return React.createElement('div',null,React.createElement('h1',null,'Does this work now'))
   }
 }
 
